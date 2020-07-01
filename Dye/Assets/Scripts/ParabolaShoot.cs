4312addf7ffefa637d28ad70785ab5a4137eb2d5
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-07-01 20:42:45 -0300

    upproject

---
+++ Dye/Assets/Scripts/ParabolaShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParabolaShoot : MonoBehaviour {
+    float _animation;
+    public GameObject Enemy;
+    public Transform posFreezy;
+    public float divisor;
+    void Start () { }
+
+    void Update () {
+        _animation += Time.deltaTime;
+        _animation = _animation % divisor;
+        transform.GetChild (0).position = MathParabola.Parabola (transform.position, posFreezy.position, 3f, _animation / divisor);
+
+        Debug.Log (MathParabola.Parabola (transform.position, posFreezy.position, 3f, _animation / divisor));
+
+        if (Vector3.Distance (transform.GetChild (0).position, posFreezy.position) < 1) {
+            Destroy (gameObject);
+        }
+    }
+}
