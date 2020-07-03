a312dc1a565af96cc730c5ed9339e30e603cc657
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-07-02 21:45:59 -0300

    Menu_Transition

---
+++ Dye/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public Animator Animation;
+    void Update () {
+
+    }
+
+    public void LoadNextLevel () {
+        StartCoroutine (LoadLevel ());
+    }
+
+    IEnumerator LoadLevel () {
+        Animation.SetTrigger ("Start");
+        yield return new WaitForSeconds (1);
+
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+
+    }
+}
